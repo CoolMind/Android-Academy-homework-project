e1e7c0d370668858c71ea38ebefdb7dabc4c8039
Alexander Nifanin <nifanin.a@uremont.com>
2020-11-22 20:43:00 +0300

    homework-02 Добавлена MovieDetailsActivity.

--- app/src/main/java/ru/arthurknight/homework/MainActivity.kt
+++ app/src/main/java/ru/arthurknight/homework/MainActivity.kt
@@ -1,11 +1,12 @@
 package ru.arthurknight.homework
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,8 +16,8 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            Snackbar.make(view, "Открыть информацию о фильме", Snackbar.LENGTH_LONG)
+                .setAction("Action") { showMovieDetailsActivity() }.show()
         }
     }
 
@@ -34,5 +35,10 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun showMovieDetailsActivity() {
+        val intent = Intent(this, MovieDetailsActivity::class.java)
+        startActivity(intent)
     }
 }
