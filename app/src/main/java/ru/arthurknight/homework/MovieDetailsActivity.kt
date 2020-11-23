7ab35a6073f2dbde517c4bd4e4a21ed0b653f9ba
Alexander Nifanin <nifanin.a@uremont.com>
2020-11-23 11:54:24 +0300

    homework-02 Добавлена разметка информации о фильме.

--- app/src/main/java/ru/arthurknight/homework/MovieDetailsActivity.kt
+++ app/src/main/java/ru/arthurknight/homework/MovieDetailsActivity.kt
@@ -1,11 +1,42 @@
 package ru.arthurknight.homework
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ru.arthurknight.homework.common.divider.DividerItemDecoration
+import ru.arthurknight.homework.common.divider.DividerItemDecoration.Companion.HORIZONTAL
 
 class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+
+        createActorsList()
+    }
+
+    private fun createActorsList() {
+        val actorsList = findViewById<RecyclerView>(R.id.movie_actors_list)
+        val divider = DividerItemDecoration(this, HORIZONTAL, false)
+        divider.setDrawable(ContextCompat.getDrawable(this, R.drawable.movie_list_divider)!!)
+        val adapter = MovieDetailsAdapter()
+        val items = arrayListOf(
+            MovieDetailsAdapter.Item(R.drawable.actor1, "Robert Downey Jr."),
+            MovieDetailsAdapter.Item(R.drawable.actor2, "Chris Evans"),
+            MovieDetailsAdapter.Item(R.drawable.actor3, "Mark Ruffalo"),
+            MovieDetailsAdapter.Item(R.drawable.actor4, "Chris Hemsworth"),
+        )
+        adapter.setItems(items)
+        with(actorsList) {
+            layoutManager = LinearLayoutManager(
+                this@MovieDetailsActivity,
+                LinearLayoutManager.HORIZONTAL,
+                false
+            )
+            this.adapter = adapter
+            addItemDecoration(divider)
+            setHasFixedSize(true)
+        }
     }
 }
