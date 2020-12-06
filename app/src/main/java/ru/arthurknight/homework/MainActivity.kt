304ce6316adeafc99ee13623369369316dc67800
Alexander Nifanin <nifanin.a@uremont.com>
2020-12-06 21:09:32 +0300

    homework-03 Изменено оформление MainActivity. Добавлен FragmentMoviesDetails.

--- app/src/main/java/ru/arthurknight/homework/MainActivity.kt
+++ app/src/main/java/ru/arthurknight/homework/MainActivity.kt
@@ -1,36 +1,15 @@
 package ru.arthurknight.homework
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Открыть информацию о фильме", Snackbar.LENGTH_LONG)
-                .setAction("Action") { showMovieDetailsActivity() }.show()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     private fun showMovieDetailsActivity() {
